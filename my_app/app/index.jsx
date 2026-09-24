581bd4fa174eff7bfcb37a0e59da9969637f6b12
ravindu <ravindulakshitha20032411@gmail.com>
2026-09-24 11:57:15 +0530

    FIrst page , learning basic component

--- my_app/app/index.jsx
+++ my_app/app/index.jsx
@@ -1,14 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Image } from 'react-native'
 import React from 'react'
+import logo from "../assets/Images/car1.jpg"
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style ={{ flex: 1, justifyContent: 'center',alignItems: 'center' ,backgroundColor:'hsl(220, 56%, 5%)'}}>
+      
+      <Text style ={[styles.text,{color :'green'}]}>This is the Main page</Text>
+      <Text style ={{color:'white'}}> Welcome !</Text>
+     <Image source={logo}  style ={styles.img} />
+
     </View>
+    
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  img :{
+     width: 200,
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin : 10,
+    padding : 10
+   
+
+  },
+  text :{
+    fontWeight : 'bold',
+    textAlignVertical : 'center',
+   
+
+  }
+})
